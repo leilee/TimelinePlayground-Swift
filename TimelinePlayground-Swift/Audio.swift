c8dd375daba227bd85cd8a0dbae202c778d91c8f
edison <ll.buptee@gmail.com>
2017-05-27 18:59:26 +0800

    Added models

---
+++ TimelinePlayground-Swift/Audio.swift
@@ -0,0 +1,9 @@
+//
+//  Audio.swift
+//  TimelinePlayground-Swift
+//
+//  Created by edison on 27/05/2017.
+//  Copyright © 2017 edison. All rights reserved.
+//
+
+import Foundation
---
+++ TimelinePlayground-Swift/Subtitle.swift
@@ -0,0 +1,9 @@
+//
+//  Subtitle.swift
+//  TimelinePlayground-Swift
+//
+//  Created by edison on 27/05/2017.
+//  Copyright © 2017 edison. All rights reserved.
+//
+
+import Foundation
